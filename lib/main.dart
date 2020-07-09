45125e2a5e4f24037032205c65521d8ad7f2ed50
Dipin KP <dipinpnambiar@gmail.com>
2020-07-09 16:18:26 +0530

    second commit,scaffold wdget used first time ,created basic app page with an appbar and body

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+//void main()=>runApp(MyApp()); The alernative syntax for function with single expression
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello Friends'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Testing Year 2020'),
+        ),
+        body: Text(
+            "Testing Year 2020Testing Year 2020Testing Year 2020Testing Year 2020Testing Year 2020Testing Year 2020vTesting Year 2020"),
+      ),
     );
   }
 }
